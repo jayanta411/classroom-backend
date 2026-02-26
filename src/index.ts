689a6c4620ecbd0960291af831cc9156dc0952d7
Jayanta <jayanta411@gmail.com>
2026-02-27 01:42:00 +0530

    implement GET all subject route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
-
+import SubjectRouter from "./routes/subjects";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+	credentials: true,
+	origin: process.env.FRONTEND_URL || "http://localhost:5173",
+	methods: ["GET", "POST", "PUT", "DELETE"],
+ }));
+app.use("/api/subjects", SubjectRouter)
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Hello from Express + TypeScript" });
 });
